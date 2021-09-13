da6b6864fc03241f8127369b530a5dd90da75693
Sowgenius <korkasow1@gmail.com>
2021-09-13 23:09:49 +0000

    Modified version of the e-transaction plugin allowing multi-site to work properly

---
+++ class/wc-etransactions-threetime-gateway.php
@@ -0,0 +1,98 @@
+<?php
+
+class WC_Etransactions_Threetime_Gateway extends WC_Etransactions_Abstract_Gateway {
+	protected $defaultTitle = 'E-Transactions 3 times payment';
+	protected $defaultDesc = 'xxxx';
+	protected $type = 'threetime';
+
+	public function __construct() {
+		// Some properties
+		$this->id = 'etransactions_3x';
+		$this->method_title = __('E-Transactions 3 times', WC_ETRANSACTIONS_PLUGIN);
+		$this->has_fields = false;
+		//$this->icon = TODO;
+
+		parent::__construct();
+	}
+
+	private function _showDetailRow($label, $value) {
+		return '<strong>'.$label.'</strong> '.$value;
+	}
+
+	/**
+	 * Check If The Gateway Is Available For Use
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function is_available() {
+		if (!parent::is_available()) {
+			return false;
+		}
+		$minimal = $this->_config->getAmount();
+		if (empty($minimal)) {
+			return true;
+		}
+		$total = WC()->cart->total;
+		$minimal = floatval($minimal);
+		return $total >= $minimal;
+	}
+
+	public function showDetails($order) {
+		$orderId = $order->id;
+		$payment = $this->_etransactions->getOrderPayments($orderId, 'first_payment');
+		if (!empty($payment)) {
+			$data = unserialize($payment->data);
+			$payment = $this->_etransactions->getOrderPayments($orderId, 'second_payment');
+			if (!empty($payment)) {
+				$second = unserialize($payment->data);
+			}
+			$payment = $this->_etransactions->getOrderPayments($orderId, 'third_payment');
+			if (!empty($payment)) {
+				$third = unserialize($payment->data);
+			}
+
+			$rows = array();
+			$rows[] = $this->_showDetailRow(__('Reference:', WC_ETRANSACTIONS_PLUGIN), $data['reference']);
+			if (isset($data['ip'])) {
+				$rows[] = $this->_showDetailRow(__('Country of IP:', WC_ETRANSACTIONS_PLUGIN), $data['ip']);
+			}
+			$rows[] = $this->_showDetailRow(__('Processing date:', WC_ETRANSACTIONS_PLUGIN), preg_replace('/^([0-9]{2})([0-9]{2})([0-9]{4})$/', '$1/$2/$3', $data['date'])." - ".$data['time']);
+			if (isset($data['firstNumbers']) && isset($data['lastNumbers'])) {
+				$rows[] = $this->_showDetailRow(__('Card numbers:', WC_ETRANSACTIONS_PLUGIN), $data['firstNumbers'].'...'.$data['lastNumbers']);
+			}
+			if (isset($data['validity'])) {
+				$rows[] = $this->_showDetailRow(__('Validity date:', WC_ETRANSACTIONS_PLUGIN), preg_replace('/^([0-9]{2})([0-9]{2})$/', '$2/$1', $data['validity']));
+			}
+
+	        $date = preg_replace('/^([0-9]{2})([0-9]{2})([0-9]{4})$/', '$1/$2/$3', $data['date']);
+	        $value = sprintf('%s (%s)', $data['amount'] / 100.0, $date);
+			$rows[] = $this->_showDetailRow(__('First debit:'), $value);
+
+			if (isset($second)) {
+	            $date = preg_replace('/^([0-9]{2})([0-9]{2})([0-9]{4})$/', '$1/$2/$3', $second['date']);
+	            $value = sprintf('%s (%s)', $second['amount'] / 100.0, $date);
+			}
+			else {
+				$value = __('Not achieved', WC_ETRANSACTIONS_PLUGIN);
+			}
+			$rows[] = $this->_showDetailRow(__('Second debit:'), $value);
+
+			if (isset($third)) {
+	            $date = preg_replace('/^([0-9]{2})([0-9]{2})([0-9]{4})$/', '$1/$2/$3', $third['date']);
+	            $value = sprintf('%s (%s)', $third['amount'] / 100.0, $date);
+			}
+			else {
+				$value = __('Not achieved', WC_ETRANSACTIONS_PLUGIN);
+			}
+			$rows[] = $this->_showDetailRow(__('Third debit:'), $value);
+
+			$rows[] = $this->_showDetailRow(__('Transaction:', WC_ETRANSACTIONS_PLUGIN), $data['transaction']);
+			$rows[] = $this->_showDetailRow(__('Call:', WC_ETRANSACTIONS_PLUGIN), $data['call']);
+			$rows[] = $this->_showDetailRow(__('Authorization:', WC_ETRANSACTIONS_PLUGIN), $data['authorization']);
+
+			echo '<h4>'.__('Payment information', WC_ETRANSACTIONS_PLUGIN).'</h4>';
+			echo '<p>'.implode('<br/>', $rows).'</p>';
+		}
+	}
+}
---
+++ woocommerce-etransactions.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Plugin Name: WooCommerce E-Transactions Payment plugin
+ * Description: E-Transactions gateway payment plugins for WooCommerce
+ * Version: 0.9.8
+ * Author: E-Transactions
+ * Author URI: http://www.e-transactions.fr
+ * 
+ * @package WordPress
+ * @since 0.9.0
+ */
+// Ensure not called directly
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+function wooCommerceActiveET(){
+	/* $wooCommerceActiveET = (in_array('woocommerce/woocommerce.php',apply_filters('active_plugins', get_option('active_plugins'))));
+  if(is_multisite()){
+    //Si multisite
+    $wooCommerceActiveET = (array_key_exists('woocommerce/woocommerce.php', 
+                            apply_filters('active_plugins', get_site_option('active_sitewide_plugins'))));
+  }*/
+ // function is_woocommerce_active(){
+      $active_plugins = (array) get_option('active_plugins', array());
+      if(is_multisite()){
+       $active_plugins = array_merge($active_plugins, get_site_option('active_sitewide_plugins', array()));
+      }
+      return in_array('woocommerce/woocommerce.php', $active_plugins) || array_key_exists('woocommerce/woocommerce.php', $active_plugins);
+ //}
+  return $wooCommerceActiveET;
+}
+
+// Ensure WooCommerce is active
+if (!wooCommerceActiveET()) {
+	return;
+}
+
+define('WC_ETRANSACTIONS_PLUGIN', 'woocommerce-etransactions');
+define('WC_ETRANSACTIONS_VERSION', '0.9.8');
+define('WC_ETRANSACTIONS_KEY_PATH', ABSPATH . '/kek.php');
+
+
+function woocommerce_etransactions_installation() {
+	global $wpdb;
+	$installed_ver = get_option( "WC_ETRANSACTIONS_PLUGIN.'_version'" );
+	
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if(!wooCommerceActiveET()) {
+		_e('WooCommerce must be activated', WC_ETRANSACTIONS_PLUGIN);
+		die();
+	}
+
+	if ( $installed_ver != WC_ETRANSACTIONS_VERSION ) {
+		$tableName = $wpdb->prefix.'wc_etransactions_payment';
+		$sql = "CREATE TABLE $tableName (
+			 id int not null auto_increment,
+			 order_id bigint not null,
+			 type enum('capture', 'first_payment', 'second_payment', 'third_payment') not null,
+			 data varchar(2048) not null,
+			 KEY order_id (order_id),
+			 PRIMARY KEY  (id))";
+
+		require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+
+		dbDelta( $sql );
+
+		update_option(WC_ETRANSACTIONS_PLUGIN.'_version', WC_ETRANSACTIONS_VERSION);
+	}
+	
+}
+function woocommerce_etransactions_initialization() {
+	$class = 'WC_Etransactions_Abstract_Gateway';
+
+	if (!class_exists($class)) {
+		require_once(dirname(__FILE__).'/class/wc-etransactions-config.php');
+		require_once(dirname(__FILE__).'/class/wc-etransactions-iso4217currency.php');
+		require_once(dirname(__FILE__).'/class/wc-etransactions.php');
+		require_once(dirname(__FILE__).'/class/wc-etransactions-abstract-gateway.php');
+		require_once(dirname(__FILE__).'/class/wc-etransactions-standard-gateway.php');
+		require_once(dirname(__FILE__).'/class/wc-etransactions-threetime-gateway.php');
+		require_once(dirname(__FILE__).'/class/wc-etransactions-encrypt.php');
+	}
+
+	load_plugin_textdomain(WC_ETRANSACTIONS_PLUGIN, false, dirname(plugin_basename(__FILE__)).'/lang/');
+
+	$crypto = new ETransactionsEncrypt();
+	if(!file_exists(WC_ETRANSACTIONS_KEY_PATH))$crypto->generateKey();
+	
+    if ( get_site_option( WC_ETRANSACTIONS_PLUGIN.'_version' ) != WC_ETRANSACTIONS_VERSION ) {
+        woocommerce_etransactions_installation();
+    }
+}
+
+function woocommerce_etransactions_register(array $methods) {
+	$methods[] = 'WC_Etransactions_Standard_Gateway';
+	$methods[] = 'WC_Etransactions_Threetime_Gateway';
+	return $methods;
+}
+
+register_activation_hook(__FILE__, 'woocommerce_etransactions_installation');
+add_action('plugins_loaded', 'woocommerce_etransactions_initialization');
+add_filter('woocommerce_payment_gateways', 'woocommerce_etransactions_register');
+
+function woocommerce_etransactions_show_details(WC_Order $order) {
+	$method = get_post_meta($order->get_id(), '_payment_method', true);
+	switch ($method) {
+		case 'etransactions_std':
+			$method = new WC_Etransactions_Standard_Gateway();
+			$method->showDetails($order);
+			break;
+		case 'etransactions_3x':
+			$method = new WC_Etransactions_Threetime_Gateway();
+			$method->showDetails($order);
+			break;
+	}
+}
+
+add_action('woocommerce_admin_order_data_after_billing_address', 'woocommerce_etransactions_show_details');
